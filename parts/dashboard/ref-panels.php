2f9d796f4ac15afee444891c93eae0f7e0925df4
Kevin Waddell <kwaddell@tlwsolicitors.co.uk>
2016-12-19 10:44:12 +0000

    Update to referrer dashboard and show office contact number for referrer.

--- parts/dashboard/ref-panels.php
+++ parts/dashboard/ref-panels.php
@@ -3,6 +3,7 @@ global $user_id;
 $current_user = wp_get_current_user();
 $user_data = get_userdata($user_id);
 $account_pg = get_page_by_path( 'account-details' );
+$cases_pg = get_page_by_path( 'cases' );
 //echo '<pre class="debug">';print_r($user_data);echo '</pre>';	
 
 $claims_args = array(
@@ -16,23 +17,22 @@ $claims = get_posts( $claims_args );
 
 $open = 0;
 $closed = 0;
-$open_cases = array();
-$closed_cases = array();
 
 	foreach ($claims as $claim) {
 	
 	$case_status = get_post_meta( $claim->ID, 'case_status', true);
+	//echo '<pre class="debug">';print_r($case_status);echo '</pre>';
 	
 		if ($case_status == 'open') {
-			$open_cases[] = $claim;	
+			$open++;
 		}
 									   		
 		if ($case_status == 'closed') {
-			$closed_cases[] = $claim;	
+			$closed++;	
 		}
 	}	
 	
-//echo '<pre class="debug">';print_r($current_user);echo '</pre>';
+//echo '<pre class="debug">';print_r($open_cases);echo '</pre>';
 //echo '<pre class="debug">';print_r($closed_cases);echo '</pre>';
 ?>
 
@@ -49,7 +49,7 @@ $closed_cases = array();
 		<div class="panel panel-success text-center">
 			<div class="panel-heading text-center">Open Cases</div>
 			<div class="panel-body">
-				<div class="number">3</div>
+				<div class="number"><?php echo $open; ?></div>
 			</div>
 		</div>
 	</div>
@@ -58,10 +58,12 @@ $closed_cases = array();
 		<div class="panel panel-danger text-center">
 			<div class="panel-heading text-center">Closed Cases</div>
 			<div class="panel-body">
-				<div class="number">3</div>
+				<div class="number"><?php echo $closed; ?></div>
 			</div>
 		</div>
 	</div>
 </div>
 
-<a href="<?php echo get_permalink( $account_pg->ID ); ?>" class="red-btn btn btn-default btn-block btn-lg">View Account details<i class="fa fa-chevron-right fa-lg pull-right"></i></a>
+<a href="<?php echo get_permalink( $cases_pg->ID ); ?>" class="red-btn btn btn-default btn-block btn-lg"><i class="fa fa-folder-open fa-lg pull-left"></i>View Your cases<i class="fa fa-chevron-right fa-lg pull-right"></i></a>
+
+<a href="<?php echo get_permalink( $account_pg->ID ); ?>" class="red-btn btn btn-default btn-block btn-lg"><i class="fa fa-vcard fa-lg pull-left"></i>View Account details<i class="fa fa-chevron-right fa-lg pull-right"></i></a>
